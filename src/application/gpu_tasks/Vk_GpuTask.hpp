5331d6c31c6987d6261282f8243c3413b595a98c
Dude19735 <dumeni.manatschal@gmail.com>
2024-09-28 03:54:10 +0200

    move Vk_GpuTask and Vk_Queue files into gpu_tasks

---
+++ src/application/gpu_tasks/Vk_GpuTask.hpp
@@ -0,0 +1,270 @@
+#pragma once
+
+#include <mutex>
+#include <condition_variable>
+#include <functional>
+#include <array>
+
+#include "../../Defines.h"
+
+namespace VK5 {
+    class Vk_GpuTask;
+    class Vk_QueueBase {
+    friend class Vk_GpuTask;
+    public:
+        virtual void _enqueueFree(VkCommandBuffer commandBuffer) = 0;
+        virtual void _enqueueSubmit(std::unique_ptr<Vk_GpuTask> task) = 0;
+    };
+
+    struct Vk_GpuTaskParams {};
+    typedef void(*TGpuTaskRecord)(VkCommandBuffer, const Vk_GpuTaskParams&);
+    typedef void(*TGpuTaskSubmit)(VkCommandBuffer, VkQueue, const Vk_GpuTaskParams&);
+    typedef void(Vk_QueueBase::*TEnqueueFree)(std::unique_ptr<Vk_GpuTask>);
+    typedef void(Vk_QueueBase::*TEnqueueSubmit)(std::unique_ptr<Vk_GpuTask>);
+
+    enum class Vk_GpuTaskStages {
+        Stage1_Alloc = 0,
+        Stage2_Record,
+        Stage3_Submit,
+        Stage4_Running,
+        Stage5_Finished,
+        Count
+    };
+
+    class Vk_Queue;
+    class Vk_GpuTask {
+    friend class Vk_Queue;
+        static constexpr int mCount = static_cast<int>(Vk_GpuTaskStages::Count);
+
+        // Thank you:
+        // https://stackoverflow.com/questions/73158786/locking-an-array-of-stdmutex-using-a-stdlock-guard-array
+        // for fancy arrays:
+        template<typename Mutexes>
+        auto makeScopedLock(Mutexes& mutexes)
+        { return std::apply([](auto&... mutexes) { return std::scoped_lock{mutexes...}; }, mutexes); }
+        
+        // // for regular arrays:
+        // template<typename Mutexes, std::size_t N, std::size_t... Is>
+        // auto makeScopedLock(Mutexes (&mutexes)[N], std::index_sequence<Is...>)
+        // { return std::scoped_lock{mutexes[Is]...}; }
+        // // helper function so you don't have to create your own index_sequence
+        // template<typename Mutexes, std::size_t N>
+        // auto makeScopedLock(Mutexes (&mutexes)[N])
+        // { return makeScopedLock(mutexes, std::make_index_sequence<N>{}); }
+
+        VkDevice _vkDevice;
+        Vk_GpuOp _opType;
+        VkFence _vkFence;
+        VkCommandBuffer _vkCommandBuffer;
+        VkCommandPool _vkCommandPool;
+
+        Vk_GpuTaskParams _params;
+        TGpuTaskRecord _recordFunction;
+        TGpuTaskSubmit _submitFunction;
+        Vk_GpuTaskStages _stage;
+
+        std::unique_ptr<Vk_GpuTask> _self;
+        Vk_QueueBase* _parentQueue;
+
+        std::array<std::mutex, mCount> _mutex;
+        std::array<std::condition_variable, mCount> _conditions;
+
+        bool _terminate;
+        std::thread _recordThread;
+        std::thread _runningWorker;
+    public:
+        Vk_GpuTask(VkDevice vkDevice, Vk_GpuOp opType) 
+        : 
+        _vkDevice(vkDevice), 
+        _opType(opType), 
+        _vkFence(_createFence(_vkDevice)), 
+        _vkCommandBuffer(nullptr), 
+        _vkCommandPool(nullptr), 
+        _params({}),
+        _recordFunction(nullptr),
+        _submitFunction(nullptr),
+        _stage(Vk_GpuTaskStages::Stage1_Alloc),
+        _self(nullptr),
+        _parentQueue(nullptr),
+        /** 
+         * TODO: figure out how to do this with variadic templates. This one is a good source
+         * https://greitemann.dev/2018/09/15/variadic-expansion-in-aggregate-initialization/ 
+        */
+        _mutex({std::mutex(), std::mutex(), std::mutex(), std::mutex(), std::mutex() }),
+        _conditions({std::condition_variable(), std::condition_variable(), std::condition_variable(), std::condition_variable(), std::condition_variable()}),
+        _terminate(false), 
+        _recordThread(std::thread(&Vk_GpuTask::_recordWorker, this)),
+        _runningWorker(std::thread(&Vk_GpuTask::_running, this))
+        {}
+        Vk_GpuTask(const Vk_GpuTask& other) = delete;
+        Vk_GpuTask(Vk_GpuTask&& other) = delete;
+        Vk_GpuTask& operator=(const Vk_GpuTask& other) = delete;
+        Vk_GpuTask& operator=(Vk_GpuTask&& other) = delete;
+
+        virtual ~Vk_GpuTask(){
+            // std::cout << ">>>>>>>>>>>>>>>>>>> destroy task" << std::endl;
+            {
+                auto scopedLock = makeScopedLock(_mutex);
+                _terminate = true;
+            }
+            for(int i=0; i<mCount; ++i) _conditions[i].notify_all();
+            if(_recordThread.joinable()) _recordThread.join();
+            if(_runningWorker.joinable()) _runningWorker.join();
+            vkDestroyFence(_vkDevice, _vkFence, nullptr);
+        }
+
+        const Vk_GpuOp opType() const { return _opType; }
+        void update(const Vk_GpuTaskParams& params, TGpuTaskRecord recordFunction, TGpuTaskSubmit submitFunction) { 
+            _params = std::move(params); 
+            if(recordFunction) _recordFunction = recordFunction;
+            if(submitFunction) _submitFunction = submitFunction;
+        }
+
+        std::unique_ptr<Vk_GpuTask> waitResponsivelyUS(std::chrono::microseconds us) {
+            // wait until signaled
+            // std::cout << "waiting for task to finish..." << std::endl;
+
+            int index = static_cast<int>(Vk_GpuTaskStages::Stage5_Finished);
+            auto lock = std::unique_lock<std::mutex>(_mutex[index]);
+            _conditions[index].wait_for(lock, us, [this](){ 
+                return _stage == Vk_GpuTaskStages::Stage5_Finished || _terminate; 
+            });
+
+            // std::cout << "   ... task finished" << std::endl;
+            return std::move(_self);
+        }
+
+        std::unique_ptr<Vk_GpuTask> waitResponsivelyMS(std::chrono::milliseconds ms) {
+            // wait until signaled
+            // std::cout << "waiting for task to finish..." << std::endl;
+
+            int index = static_cast<int>(Vk_GpuTaskStages::Stage5_Finished);
+            auto lock = std::unique_lock<std::mutex>(_mutex[index]);
+            _conditions[index].wait_for(lock, ms, [this](){ 
+                return _stage == Vk_GpuTaskStages::Stage5_Finished || _terminate; 
+            });
+
+            // std::cout << "   ... task finished" << std::endl;
+            return std::move(_self);
+        }
+
+        std::unique_ptr<Vk_GpuTask> waitResponsively() {
+            // wait until signaled
+            // std::cout << "waiting for task to finish..." << std::endl;
+
+            int index = static_cast<int>(Vk_GpuTaskStages::Stage5_Finished);
+            auto lock = std::unique_lock<std::mutex>(_mutex[index]);
+            _conditions[index].wait(lock, [this](){ 
+                return _stage == Vk_GpuTaskStages::Stage5_Finished || _terminate; 
+            });
+
+            std::cout << "   ... task finished" << std::endl;
+            return std::move(_self);
+        }
+
+    private:
+        VkFence fence() { return _vkFence; }
+
+        void resetStage(){ _stage = Vk_GpuTaskStages::Stage1_Alloc; }
+
+        // stage 1 and 5 (serialized for VkCommandPool)
+        void passAlloc(std::unique_ptr<Vk_GpuTask> self, VkCommandBuffer commandBuffer, Vk_QueueBase* pParentQueue){
+            if(_vkCommandBuffer != nullptr) UT::Ut_Logger::RuntimeError(typeid(this), "CommandBuffer is not nullptr! Alloc before free => this is a bug!");
+            _vkCommandBuffer = commandBuffer;
+            _self = std::move(self);
+            _parentQueue = pParentQueue;
+
+            // goto next
+            _stage = Vk_GpuTaskStages::Stage2_Record;
+            _conditions[static_cast<int>(Vk_GpuTaskStages::Stage2_Record)].notify_one();
+            // std::cout << "T1." << std::endl;
+        }
+
+        void _recordWorker(){
+            // std::cout << ">>>>>>>>>>>>>>>>>>> Start record worker" << std::endl;
+            // run endlessly... we should techincally get one task at the time
+            int index = static_cast<int>(Vk_GpuTaskStages::Stage2_Record);
+            while(true){
+                {
+                    auto lock = std::unique_lock<std::mutex>(_mutex[index]);
+                    _conditions[index].wait(lock, [this](){
+                        return _stage == Vk_GpuTaskStages::Stage2_Record || _terminate;
+                    });
+
+                    if(_terminate){
+                        // std::cout << ">>>>>>>>>>>>>>>>>>> Stop record worker" << std::endl;
+                        return;
+                    }
+
+                    // std::cout << "T2." << std::endl;
+                    if(_recordFunction) _recordFunction(_vkCommandBuffer, _params);
+
+                    // goto next: back to Vk_Queue because this one has to be in sync
+                    _stage = Vk_GpuTaskStages::Stage3_Submit;
+                }
+                /**
+                 * NOTE: make sure all locks are released before calling this one
+                 * _enqueueSubmit is thread safe
+                 */
+                _parentQueue->_enqueueSubmit(std::move(_self));
+            }
+        }
+
+        void submit(std::unique_ptr<Vk_GpuTask> self, VkQueue vkQueue) {
+            // submit task, run from Vk_Queue
+            // std::cout << "T3." << std::endl;
+            if(_submitFunction) _submitFunction(_vkCommandBuffer, vkQueue, _params);
+            _self = std::move(self);
+
+            // goto next
+            _stage = Vk_GpuTaskStages::Stage4_Running;
+            _conditions[static_cast<int>(Vk_GpuTaskStages::Stage4_Running)].notify_one();
+        }
+
+        void _running() {
+            // std::cout << ">>>>>>>>>>>>>>>>>>> Start task runner" << std::endl;
+            int index = static_cast<int>(Vk_GpuTaskStages::Stage4_Running);
+            int finished = static_cast<int>(Vk_GpuTaskStages::Stage5_Finished);
+            while(true) {
+                {
+                    auto lock = std::unique_lock<std::mutex>(_mutex[index]);
+                    _conditions[index].wait(lock, [this](){
+                        return _stage == Vk_GpuTaskStages::Stage4_Running || _terminate;
+                    });
+
+                    if(_terminate){
+                        // std::cout << ">>>>>>>>>>>>>>>>>>> Stop task runner" << std::endl;
+                        return;
+                    }
+                    
+                    // std::cout << "T4." << std::endl;
+                    VkResult res = vkWaitForFences(_vkDevice, 1, &_vkFence, VK_TRUE, GLOBAL_FENCE_TIMEOUT);
+                    if(res == VK_TIMEOUT) UT::Ut_Logger::RuntimeError(typeid(this), "Signal timeout");
+                    else if (res != VK_SUCCESS) UT::Ut_Logger::RuntimeError(typeid(this), "Signal catastrophic result!");
+
+                    // std::cout << "T4.." << std::endl;
+                    _parentQueue->_enqueueFree(_vkCommandBuffer);
+                    _vkCommandBuffer = nullptr;
+                    _parentQueue = nullptr;
+
+                    _stage = Vk_GpuTaskStages::Stage5_Finished;
+                }
+                /**
+                 * NOTE: make sure all locks are released before calling notify
+                 */
+                _conditions[finished].notify_all(); // everyone who is waiting
+            }
+        }
+
+        VkFence _createFence(VkDevice vkDevice){
+            VkFenceCreateInfo fenceInfo{};
+            fenceInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+            // we must create the fences in the signaled state to ensure that on the first call to drawFrame
+            // vkWaitForFences won't wait indefinitely
+            fenceInfo.flags = VK_FENCE_CREATE_SIGNALED_BIT;
+            VkFence fence;
+            Vk_CheckVkResult(typeid(this), vkCreateFence(vkDevice, &fenceInfo, nullptr, &fence), "Failed to create in flight fences");
+            return fence;
+        }
+    };
+}
---
+++ src/application/gpu_tasks/Vk_Queue.hpp
@@ -0,0 +1,207 @@
+#pragma once
+
+#include <thread>
+#include <queue>
+#include <set>
+#include <condition_variable>
+
+#include "../../Defines.h"
+#include "../../Vk_CI.hpp"
+#include "../Vk_PhysicalDeviceQueueLib.hpp"
+#include "Vk_GpuTask.hpp"
+
+namespace VK5 {
+    class Vk_Queue : public Vk_QueueBase {
+    private:
+        VkDevice _vkDevice;
+        TQueueFamilyIndex _familyIndex;
+        TQueueIndex _queueIndex;
+        VkQueue _vkQueue;
+        VkCommandPool _vkCommandPool;
+
+        bool _terminate;
+
+        /**
+         * NOTE: the sequence of these variables is important. They are initialized exactly
+         * in the sequence they are written. If _allocFreeThread starts running before _allocFreeCondition
+         * and/or _allocFreeMutex are fully initialized, the whole thing won't work!!!
+         */
+        std::queue<std::unique_ptr<Vk_GpuTask>> _allocTasks;
+        std::queue<VkCommandBuffer> _usedCommandBuffers;
+        std::mutex _allocFreeMutex;
+        std::condition_variable _allocFreeCondition;
+        std::thread _allocFreeThread;
+
+        /**
+         * NOTE: the sequence of these variables is important. They are initialized exactly
+         * in the sequence they are written. First, write the queue, then the mutex, then the
+         * condition_variable and at the very end, the thread. This will guarantee, that every
+         * part has what they need up and running in time.
+         */
+        std::queue<std::unique_ptr<Vk_GpuTask>> _submitTasks;
+        std::mutex _submitMutex;
+        std::condition_variable _submitCondition;
+        std::thread _submitThread;
+
+    public:
+        // regular constructor
+        Vk_Queue(VkDevice vkDevice, uint32_t familyIndex, uint32_t queueIndex) 
+        : 
+        _vkDevice(vkDevice), _familyIndex(familyIndex), _queueIndex(queueIndex),
+        _vkQueue(_getDeviceQueue(_vkDevice, _familyIndex, _queueIndex)), _vkCommandPool(_createCommandPool(_vkDevice, _familyIndex)),
+        _terminate(false), 
+        _allocFreeThread(std::thread(&Vk_Queue::_allocFreeLoop, this)),
+        _submitThread(std::thread(&Vk_Queue::_submitLoop, this))
+        {}
+
+        Vk_Queue(const Vk_Queue& other) = delete;
+        Vk_Queue(Vk_Queue&& other) = delete;
+        Vk_Queue& operator=(const Vk_Queue& other) = delete;
+        Vk_Queue& operator=(Vk_Queue&& other) = delete;
+
+        ~Vk_Queue(){
+            {
+                std::scoped_lock lock {_allocFreeMutex, _submitMutex };
+                _terminate = true;
+            }
+            /**
+             * NOTE: make sure all the locks are released before notifying!
+             */
+            _allocFreeCondition.notify_one();
+            if(_allocFreeThread.joinable()) _allocFreeThread.join();
+            _submitCondition.notify_one();
+            if(_submitThread.joinable()) _submitThread.join();
+
+            // std::cout << "join " << toString() << std::endl;
+
+            if(_vkQueue != nullptr) vkQueueWaitIdle(_vkQueue);
+            while(!_usedCommandBuffers.empty()){
+                VkCommandBuffer b = _usedCommandBuffers.front();
+                vkFreeCommandBuffers(_vkDevice, _vkCommandPool, 1, &b);
+                _usedCommandBuffers.pop();
+            }
+            if(_vkCommandPool != nullptr) vkDestroyCommandPool(_vkDevice, _vkCommandPool, nullptr);
+        }
+
+        const TQueueFamilyIndex familyIndex() const { return _familyIndex; }
+        const TQueueIndex queueIndex() const { return _queueIndex; }
+
+        std::string toString() const {
+            std::stringstream ss;
+            ss << _familyIndex << "|" << _queueIndex;
+            return ss.str();
+        }
+
+        Vk_GpuTask* enqueue(std::unique_ptr<Vk_GpuTask> task){
+            Vk_GpuTask* res;
+            {
+				std::unique_lock<std::mutex> lock(_allocFreeMutex);
+                task->resetStage();
+                res = task.get();
+                _allocTasks.push(std::move(task));
+			}
+			_allocFreeCondition.notify_one();
+            return res;
+        }
+
+    private:
+        void _enqueueFree(VkCommandBuffer commandBuffer){
+            {
+				std::unique_lock<std::mutex> lock(_allocFreeMutex);
+                _usedCommandBuffers.push(commandBuffer);
+			}
+			_allocFreeCondition.notify_one();
+        }
+
+        void _allocFreeLoop() {
+            // std::cout << "############################### Start passAlloc loop for " << toString() << std::endl;
+            std::unique_ptr<Vk_GpuTask> cTask = nullptr;
+            VkCommandBuffer vkCommandBuffer;
+            while(true){
+                {
+                    auto lock = std::unique_lock<std::mutex>(_allocFreeMutex);
+                    _allocFreeCondition.wait(lock, [this](){
+                        return !_allocTasks.empty() || !_usedCommandBuffers.empty() || _terminate;
+                    });
+                    
+                    if(_terminate){
+                        std::queue<std::unique_ptr<Vk_GpuTask>> empty;
+                        std::swap(_allocTasks, empty);
+                        // std::cout << "############################### Stop passAlloc loop for " << toString() << std::endl;
+                        return;
+                    }
+
+                    // first check, if we have some task that is finished and deposited it's command buffer
+                    // back here.
+                    if(_allocTasks.size() > 0){
+                        // std::cout << _usedCommandBuffers.size() << std::endl;            
+                        if(!_usedCommandBuffers.empty()){
+                            vkCommandBuffer = _usedCommandBuffers.front();
+                            _usedCommandBuffers.pop();
+                            // std::cout << " Q1n" << std::endl;
+                        }
+                        else {
+                            // std::cout << " Q1r" << std::endl;
+                            // if we don't have a spare command buffer, get one from the pool
+                            // technically, the cost should ammortize, but maybe it doesn't, so reuse rather than get new ones
+                            auto createInfo = Vk_CI::VkCommandBufferAllocateInfo_W(1, _vkCommandPool).data;
+                            Vk_CheckVkResult(typeid(this), vkAllocateCommandBuffers(_vkDevice, &createInfo, &vkCommandBuffer), "Failed to allocate command buffers!");
+                        }
+                        cTask = std::move(_allocTasks.front());
+                        _allocTasks.pop();
+                        // std::cout << " Q1s" << std::endl;
+                    }
+                }
+                if(cTask) cTask->passAlloc(std::move(cTask), vkCommandBuffer, this);
+            }
+        }
+
+        void _enqueueSubmit(std::unique_ptr<Vk_GpuTask> task){
+            {
+				std::unique_lock<std::mutex> lock(_submitMutex);
+                _submitTasks.push(std::move(task));
+			}
+			_submitCondition.notify_one();
+        }
+
+        void _submitLoop() {
+            // std::cout << "############################### Start submit loop for " << toString() << std::endl;
+            std::unique_ptr<Vk_GpuTask> cTask = nullptr;
+            while(true){
+                {
+                    auto lock = std::unique_lock<std::mutex>(_submitMutex);
+                    _submitCondition.wait(lock, [this](){
+                        return !_submitTasks.empty() || _terminate;
+                    });
+                    
+                    if(_terminate){
+                        // std::cout << "############################### Stop submit loop for " << toString() << std::endl;
+                        std::queue<std::unique_ptr<Vk_GpuTask>> empty;
+                        std::swap(_submitTasks, empty);
+                        return;
+                    }
+                    
+                    cTask = std::move(_submitTasks.front());
+                    _submitTasks.pop();
+                    // std::cout << " Q2." << std::endl;
+                }
+                if(cTask) cTask->submit(std::move(cTask), _vkQueue);
+            }
+        }
+
+        VkQueue _getDeviceQueue(VkDevice vkDevice, TQueueFamilyIndex familyIndex, TQueueIndex queueIndex) {
+            VkQueue vkQueue;
+            // get the queue from the device
+            vkGetDeviceQueue(vkDevice, familyIndex, queueIndex, &vkQueue);
+            return vkQueue;
+        }
+
+        VkCommandPool _createCommandPool(VkDevice vkDevice, TQueueFamilyIndex familyIndex) {
+            VkCommandPool vkCommandPool;
+            // create command pool for the device graphics queue
+            auto cmCreateInfo = Vk_CI::VkCommandPoolCreateInfo_W(familyIndex).data;
+            Vk_CheckVkResult(typeid(NoneObj), vkCreateCommandPool(_vkDevice, &cmCreateInfo, nullptr, &vkCommandPool), std::string("Unable to create command pool for queue ") + Vk_Queue::toString());
+            return vkCommandPool;
+        }
+    };
+}
